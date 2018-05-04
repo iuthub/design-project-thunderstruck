f3c31da93909a7443843f5cd9ec11fcfebcba6fc
Tessium <the.phenomenon@mail.ru>
2018-05-05 01:50:48 +0500

    Fixed menu error, removed junk from map

--- js/script.js
+++ js/script.js
@@ -64,7 +64,7 @@
 
                     // How you would like to style the map. 
                     // This is where you would paste any style found on Snazzy Maps.
-                    styles: [{"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}]
+                    styles: [{"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},			{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}]
                 };
 
                 // Get the HTML DOM element that will contain your map 
@@ -76,9 +76,8 @@
 
                 // Let's also add a marker while we're at it
                 var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(67.1547422, 26.9111954),
+                    position: new google.maps.LatLng(67.1547422, 26.9122054),
                     map: map,
-                    title: 'Snazzy!'
                 });
             }
 
@@ -86,13 +85,13 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("fullpage").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("fullpage").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
