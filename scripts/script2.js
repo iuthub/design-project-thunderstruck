cc530b1cbe7debdc978cfc2196f7312160eadb25
Tessium <the.phenomenon@mail.ru>
2018-05-14 23:03:22 +0500

    Cleared for full app

--- scripts/script2.js
+++
@@ -1,8 +0,0 @@
-// $(function(){
-// 	$('.form-control').focus(function(){
-// 		$(this).addClass('form-control-borderC');
-// 	}).blur(function(){
-// 		$(this).removeClass('form-control-borderC');
-// 	});
-// });
-//   
